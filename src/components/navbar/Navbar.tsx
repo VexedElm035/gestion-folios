5ad81f509f97afefa348999bb9465f890da5b879
VexedElm035 <luisdelrazo035@gmail.com>
2026-01-14 22:27:45 -0600

    Responsiveness style for Table

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { IoIosSearch } from "react-icons/io";
 import { IoIosMore } from "react-icons/io";
+import { IoMdPerson } from "react-icons/io";
+import { IoMdStopwatch } from "react-icons/io";
+import { IoMdSettings } from "react-icons/io";
 import Options from '../options/Options';
 import RunnerButton from './elements/RunnerButton';
 import './navbar.css';
 
 
 const navbarItems = [
-    { nombre: 'Participantes', enlace: '/' },
-    { nombre: 'Cronometraje', enlace: '/timing' },
-    { nombre: 'Configuraciones', enlace: '/settings' },
+    { nombre: 'Participantes', enlace: '/', icon: IoMdPerson },
+    { nombre: 'Cronometraje', enlace: '/timing', icon: IoMdStopwatch },
+    { nombre: 'Configuraciones', enlace: '/settings',  icon: IoMdSettings },
 ];
 
 const Navbar = () => {
@@ -18,18 +21,21 @@ const Navbar = () => {
   const [optionsMenu, setOptionsMenu] = useState(false);
   return (
     <div className='navbar'>
-        <section>
-            <div className='navbar-items'>
-                {navbarItems.map((item, index) => (
-                    <div 
-                        key={index} 
-                        className={`navbar-item ${location.pathname === item.enlace ? 'active' : ''}`}
-                    >
-                        <a href={item.enlace}>{item.nombre}</a>
-                    </div>
-                ))}
+      <section>
+        <div className='navbar-items'>
+          {navbarItems.map((item, index) => (
+            <div key={index} 
+            className={`navbar-item ${location.pathname === item.enlace ? 'active' : ''}`}>
+              <a href={item.enlace}>
+                <item.icon className='navbar-item-icon'/>
+                <span>
+                  {item.nombre}
+                </span> 
+              </a>
             </div>
-        </section>
+          ))}
+        </div>
+      </section>
 
         <section>
           <RunnerButton />
