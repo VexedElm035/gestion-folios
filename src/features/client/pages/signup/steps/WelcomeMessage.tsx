792b734a63220b00905f98d188f3a1604f4c666a
VexedElm035 <luisdelrazo035@gmail.com>
2026-01-30 12:09:00 -0600

    Styles Fix & Improvements

--- src/features/client/pages/signup/steps/WelcomeMessage.tsx
+++ src/features/client/pages/signup/steps/WelcomeMessage.tsx
@@ -5,8 +5,6 @@ const WelcomeMessage = ({ goNext } : { goNext: () => void }) => {
       <h1 className="signup-header-text">Hola!</h1>
       <h2 className="signup-header-steps-text">¡Bienvenido al proceso de registro de la 2da. Carrera a la Frontera!</h2>
       <p>mensaje</p>
-      <p>mensaje</p>
-      <p>aaaaaaaaaaaaaaaa</p>
       <button className='signup-navigation-button signup-navigation-button-next' onClick={goNext} aria-label="Siguiente">
           <p>Siguiente</p>
           {/* <IoIosArrowForward size={30} /> */}
